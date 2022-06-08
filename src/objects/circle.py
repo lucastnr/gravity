0611020da5b05136c4beea7dff5eb279fb524175
Lucas <lucas.ol.tnr@gmail.com>
2022-06-08 16:30:24 -0300

    Update initial circle state

--- src/objects/circle.py
+++ src/objects/circle.py
@@ -10,12 +10,12 @@ class Circle(Moveable):
 
   def __init__(self, radius: float):
     self.radius = radius
-    super().__init__([450, 240], (8000, 0), (0, 0), 0)
+    super().__init__([450, 220], (7300, 0), (0, 5), 0)
 
   def render(self, paused: bool):
     if not paused:
       self.update_position()
-    print("Distance to Earth: ", self.earth_distance)
+    print("Distance to Earth: ", self.earth_distance - earth_size/2)
     pygame.draw.circle(screen, (0, 255, 0),
                    self.position, self.radius)
 
